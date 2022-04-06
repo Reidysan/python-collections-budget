09aa6f150b57452d564f93d5602688252dd40504
Callum Reid <callum.reid92@hotmail.com>
2022-04-06 21:27:56 +1000

    Completed module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -14,7 +14,7 @@ def main():
     for a,b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
-        print(timeit.timeit(stmt= 'expenses.categories_for_loop()',
+        print(timeit.timeit(stmt= 'expenses.categorize_for_loop()',
         setup=
         '''
         from . import Expense
@@ -25,7 +25,7 @@ def main():
         globals=globals(),
         ))
 
-        print(timeit.timeit(stmt= 'expenses.categories_set_comprehension()',
+        print(timeit.timeit(stmt= 'expenses.categorize_set_comprehension()',
         setup=
         '''
         from . import Expense
