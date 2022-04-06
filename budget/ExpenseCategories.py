dbb11e16c5dea98dca062fd8c69fe25c40783c44
Callum Reid <callum.reid92@hotmail.com>
2022-04-06 21:29:37 +1000

    Completed module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -17,23 +17,23 @@ def main():
         print(timeit.timeit(stmt= 'expenses.categorize_for_loop()',
         setup=
         '''
-        from . import Expense
-        expenses = Expense.Expenses()
-        expenses.read_expenses("data/spending_data.csv")
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses("data/spending_data.csv")
         ''' ,
         number=100000,
-        globals=globals(),
+        globals=globals()
         ))
 
         print(timeit.timeit(stmt= 'expenses.categorize_set_comprehension()',
         setup=
         '''
-        from . import Expense
-        expenses = Expense.Expenses()
-        expenses.read_expenses("data/spending_data.csv")
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses("data/spending_data.csv")
         ''' ,
         number=100000,
-        globals=globals(),
+        globals=globals()
         ))
 
     fig,ax=plt.subplots()
